0978383af3d1c07e384d78f1f5155a0f67743a68
Suman Panigrahi <psuman7155@gmail.com>
2026-07-03 10:10:21 +0530

    Fix CORS policy to allow Codespace URLs

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,14 +15,13 @@ app = FastAPI(
 )
 
 # Set up CORS
-if settings.BACKEND_CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_db_client():
